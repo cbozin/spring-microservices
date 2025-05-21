06b27eb4054f160b896bea345acff9e36a98b3e8
Carolyn Bozin <carolynbozhinov@gmail.com>
2025-05-20 18:58:12 -0700

    updating product_service

--- order-service/src/main/java/com/caracal/microservices/order/service/OrderService.java
+++ order-service/src/main/java/com/caracal/microservices/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.caracal.microservices.order.service;
 
+import com.caracal.microservices.order.client.InventoryClient;
 import com.caracal.microservices.order.dto.OrderRequest;
 import com.caracal.microservices.order.model.Order;
 import com.caracal.microservices.order.repository.OrderRepository;
@@ -13,16 +14,25 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
-    public void placeOrder(OrderRequest orderRequest){
-        //map order request to order object
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setPrice(orderRequest.price());
-        order.setSkuCode(orderRequest.skuCode());
-        order.setQuantity(orderRequest.quantity());
-        //save order to OrderRepository
-        orderRepository.save(order);
+    public void placeOrder(OrderRequest orderRequest) {
+
+        var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+
+        if (isProductInStock) {
+            //map order request to order object
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setPrice(orderRequest.price());
+            order.setSkuCode(orderRequest.skuCode());
+            order.setQuantity(orderRequest.quantity());
+            //save order to OrderRepository
+            orderRepository.save(order);
+        }else {
+            throw new RuntimeException("Product with skuCode " + orderRequest.skuCode() + " is not in stock");
+        }
+
     }
 
 
